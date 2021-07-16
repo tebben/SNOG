ff0cab6504d518fb5d4272de99ba3e6726c46fe6
Tim Ebben <time@bak.localdomain>
2021-07-16 10:19:48 +0200

    fix paths for linux, add missing requirements

--- set_path.py
+++ set_path.py
@@ -6,11 +6,11 @@ Created on Mon Mar 29 19:02:56 2021
 """
 
 class path:
-    path_to_meta = r"meta\21.03.29_Optimization Development Calculations_M.Ghodsvali.xlsx"
-    path_to_landuse = r"landuse\landuse.tif"
-    path_to_norm_vals = r"meta\norm_vals.csv"
+    path_to_meta = r"meta/21.03.29_Optimization Development Calculations_M.Ghodsvali.xlsx"
+    path_to_landuse = r"landuse/landuse.tif"
+    path_to_norm_vals = r"meta/norm_vals.csv"
     path_to_output = r"output"
-    path_to_optimized = r"Best scenarios\Nexus optimized scenario\NSGA_II_Nexus optimized scenario_pop550_gen260_errorbuffer0.0_Edited_WeightedCompat\out.xlsx"
+    path_to_optimized = r"Best scenarios/Nexus optimized scenario/NSGA_II_Nexus optimized scenario_pop550_gen260_errorbuffer0.0_Edited_WeightedCompat/out.xlsx"
     
 class sheets:
     configs = 'Configs'
